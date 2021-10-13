d6d623e18e7b02ca13072c36faff40605ff2ad9a
bielma <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>
2021-10-13 11:03:39 -0500

    lista de categorias flotante

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,14 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Category } from '../Category'
 
 import { List, Item } from './styles'
 
+function useCategoriesData() {
+  const [categories, setCategories] = useState([])
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    setLoading(true)
+    window.fetch(
+      'https://petgram-server-edsf8xpy2.now.sh/categories'
+    ).then(res => res.json())
+      .then(response => setCategories(response))
+    setLoading(false)
+  }, [])
+
+  return { categories, loading };
+}
+
 export const ListOfCategories = () => {
-  return (
-    <List>
+  const { categories, loading } = useCategoriesData()
+  const [showFixed, setShowFixed] = useState(false)
+
+
+
+  useEffect(() => {
+    const onScroll = e => {
+      const show = window.scrollY > 200
+      showFixed != show &&
+        setShowFixed(show)
+    }
+    document.addEventListener('scroll', onScroll)
+    return () => document.removeEventListener('scrol', onScroll)
+  }, [showFixed])
+  const renderList = (fixed) => (
+    <List fixed={fixed}>
       {
-        [1, 2, 3, 4].map(category => <Item key={category}><Category /></Item>)
+        categories.map(category => <Item key={category.id}><Category   {...category} /></Item>)
       }
     </List>
   )
+  if (loading) {
+    return (<h1>Cargando...</h1>)
+  } else {
+
+    return (
+      <>
+        {renderList()}
+        {showFixed && renderList(true)}
+      </>
+    )
+  }
 }
