e761609e15f70d059fb4628a0b21cb58da29452e
bielma <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>
2021-10-16 13:19:12 -0500

    lazy load

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,23 +1,48 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Category } from '../Category'
-import { ImgWrapper, Button, Img } from './styles'
+import { ImgWrapper, Button, Img, Article } from './styles'
 //import { MdFavoriteBorder } from 'react-icons'
 
 const DEFAULT_IMAGE = "https://res.cloudinary.com/midudev/image/upload/w_150/v1555671700/category_cats.jpg"
 
-export const PhotoCard = ({id, likes = 0 , src= DEFAULT_IMAGE}) => {
-    console.log('object')
+export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+    const element = useRef(null)
+    const [show, setShow] = useState(false)
+    useEffect(() => {
+        import('intersection-observer')
+        .then(()=>{
+            const observer = new IntersectionObserver(entries => {
+            
+                const { isIntersecting } = entries[0]
+                console.log(isIntersecting)
+                if (isIntersecting) {
+                    setShow(true)
+                    observer.disconnect()
+                }
+    
+            })
+            observer.observe(element.current)
+        })
+        
+
+    }, [element])
+
     return (
-        <article>
-            <a href = {`/detail/${id}`} >
-                <ImgWrapper>
-                    <Img src  = {src}/>
-                </ImgWrapper>
-            </a>
-            <Button>
-                
-            </Button>
-        </article>
+        <Article ref={element}>
+            {
+                show &&
+                <>
+                    <a href={`/detail/${id}`} >
+                        <ImgWrapper>
+                            <Img src={src} />
+                        </ImgWrapper>
+                    </a>
+                    <Button>
+
+                    </Button>
+                </>
+            }
+        </Article>
     )
 }
 
